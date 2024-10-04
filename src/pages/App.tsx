7b9e44789ac9e980f8dc88f513deae67471cc680
Jonathan Ramirez <jonajramirez@gmail.com>
2024-10-03 23:28:40 -0600

    init: Add init skeleton to project

---
+++ src/pages/App.tsx
@@ -0,0 +1,11 @@
+const App = () => {
+  return (
+    <>
+      <p className="text-1xl font-bold underline text-center text-cyan-50">
+        Init Template React
+      </p>
+    </>
+  )
+}
+
+export default App;
